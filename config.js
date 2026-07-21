22a580b13b48ced156a9aad4f95893632913e1db
Cappy-2024 <robloxgamerpro17@gmail.com>
2026-07-20 20:21:01 -0700

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,10 @@
-export const SUPABASE_URL = "https://aaasylkzlmzrgqnqpncm.supabase.co";
+const SUPABASE_URL = "https://aaasylkzlmzrgqnqpncm.supabase.co";
 
-export const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFhYXN5bGt6bG16cmdxbnFwbmNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1NzU4NTcsImV4cCI6MjEwMDE1MTg1N30.MBhgdpu6y81AjdwH8ZXgOtM_2jnE4N85E47c9BWPmmQ";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFhYXN5bGt6bG16cmdxbnFwbmNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1NzU4NTcsImV4cCI6MjEwMDE1MTg1N30.MBhgdpu6y81AjdwH8ZXgOtM_2jnE4N85E47c9BWPmmQ";
+
+const supabaseClient = supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
+
+console.log("Supabase Client:", supabaseClient);
