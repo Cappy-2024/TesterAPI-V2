25d057ce93d92c1b35a1868dd227b9e02f34b925
Cappy-2024 <robloxgamerpro17@gmail.com>
2026-07-20 20:16:02 -0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,9 @@ const SUPABASE_URL = "https://aaasylkzlmzrgqnqpncm.supabase.co";
 
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFhYXN5bGt6bG16cmdxbnFwbmNtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ1NzU4NTcsImV4cCI6MjEwMDE1MTg1N30.MBhgdpu6y81AjdwH8ZXgOtM_2jnE4N85E47c9BWPmmQ";
 
-const supabaseClient = supabase.createClient(
+import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+
+const supabaseClient = createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
