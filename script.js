5e0196eb4475d4ced6a750523a38f0b26ac8a67a
Cappy-2024 <robloxgamerpro17@gmail.com>
2026-07-20 17:04:28 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+const searchInput = document.getElementById("searchInput");
 
+const searchButton = document.getElementById("searchButton");
+
+searchButton.onclick = () => {
+
+    const text = searchInput.value.trim();
+
+    if(text.length === 0){
+
+        alert("Enter a username or UserId.");
+
+        return;
+
+    }
+
+    console.log(text);
+
+};
